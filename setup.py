1917b334ba6dbf3188b43f5bc6985692488a382e
Ivar Grytten <ivar.grytten@gmail.com>
2023-02-27 10:32:33 +0100

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='numpy_alignments',
-      version='0.0.4',
+      version='0.0.5',
       description='Numpy Alignments',
       url='http://github.com/ivargr/numpy_alignments',
       author='Ivar Grytten',
